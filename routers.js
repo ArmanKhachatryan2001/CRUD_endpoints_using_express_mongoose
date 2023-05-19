d07bfb722b5774bcd9d8edb3ffee698eb38c5577
Arman <armkhachatryan453@gmail.com>
2023-05-19 18:44:28 +0400

    add

--- routers.js
+++ routers.js
@@ -4,13 +4,15 @@ const controllers = require('./controllers');
 
 const {
     createdata,
+    updatealldata,
     updatedata,
     deletedata,
     fetchdata
 } = controllers;
 
 router.post('/post', createdata);
-router.put('/update/:id', updatedata);
+router.put('/update/:id', updatealldata);
+router.patch('/update/:id', updatedata);
 router.delete('/del/:id', deletedata);
 router.get('/fetch/:id', fetchdata);
 
